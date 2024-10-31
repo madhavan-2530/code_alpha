030a52e5e6a9f4368b51e5e7126a1356b60a57e6
madhavan-2530 <jaya200504@gmail.com>
2024-10-31 12:16:13 +0530

    Add files via upload

---
+++ madhavan-calculator/calc.js
@@ -0,0 +1,18 @@
+const display1 = document.getElementById("display");
+let dval=display1.value;
+
+function appendtodisplay(input){
+    display1.value += input;
+
+}
+
+function calculate(){
+   try{
+    display1.value=eval(display1.value)
+   }
+   catch(error){
+     display1.value="Error"
+   }
+   
+  }
+
